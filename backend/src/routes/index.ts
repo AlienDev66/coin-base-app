1f55e425de35d5f8b61c0d25969f41c472b90bd1
jmamadeu <jmamadeu2000@gmail.com>
2020-09-06 14:31:51 +0100

    :fire: chore: Add session route and refactored user route

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,21 +1,34 @@
 import { Router } from 'express';
 
-import { UserController } from '../controllers';
+import { UserController, SessionController } from '../controllers';
 
 const userController = new UserController();
+const sessionController = new SessionController();
 
 const routes = Router();
 
-routes.get('/users', async (_, res) => {
+routes.get('/users', async (_, response) => {
   const userResponse = await userController.index();
 
-  return res.status(userResponse.statusCode).json(userResponse.content);
+  return response.status(userResponse.statusCode).json(userResponse.content);
 });
 
-routes.post('/users', async (req, res) => {
-  const userResponse = await userController.create({ request: req });
+routes.post('/users', async (request, response) => {
+  const { name, password, email } = request?.body;
 
-  return res.status(userResponse.statusCode).json(userResponse.content);
+  const userResponse = await userController.create({ name, password, email });
+
+  return response.status(userResponse.statusCode).json(userResponse.content);
+});
+
+routes.post('/sessions', async (request, response) => {
+  const { email, password } = request?.body;
+
+  const sessionResponse = await sessionController.create({ email, password });
+
+  return response
+    .status(sessionResponse?.statusCode)
+    .json(sessionResponse?.content);
 });
 
 export default routes;
