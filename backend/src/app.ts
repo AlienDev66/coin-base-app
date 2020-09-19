5f9405e06c9082718ee245f59199e146b6f4c252
jmamadeu <jmamadeu2000@gmail.com>
2020-09-19 14:01:29 +0100

    :sparkles: feature: Serve static files

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,4 +1,6 @@
 import 'reflect-metadata';
+
+import path from 'path';
 import express from 'express';
 
 import { typeOrmConnection } from './database';
@@ -9,6 +11,11 @@ const app = express();
 typeOrmConnection
   .then((database) => console.log('Database Success', database.name))
   .catch((err) => console.log('Database Error', err.message));
+
+app.use(
+  '/files',
+  express.static(path.join(__dirname, '..', 'tmp', 'assets', 'svg-cards'))
+);
 
 app.use(express.json());
 app.use(routes);
